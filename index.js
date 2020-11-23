3a78c889659c6612aa480766ec40c3a1a91477ff
exmachiana <72504253+exmachiana@users.noreply.github.com>
2020-11-22 23:30:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://chisato.glitch.me"]
+const urls = ["https://chisanto-bot-.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
